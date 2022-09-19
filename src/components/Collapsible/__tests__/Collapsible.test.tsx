8c5f2a5866dd4d5582148851760f53377e7e81ce
JoniRinta-Kahila <joni.rintakahila@opp.eduvantaa.fi>
2022-09-19 22:55:24 +0300

    test(collapsible): create tests

--- src/components/Collapsible/__tests__/Collapsible.test.tsx
+++ src/components/Collapsible/__tests__/Collapsible.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Collapsible, CollapsibleProps } from '..';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 const defaultProps: CollapsibleProps = {};
 
@@ -8,7 +8,32 @@ const setup = (props = defaultProps) => render(<Collapsible {...props} />);
 
 describe('Collapsible', () => {
   it('renders', () => {
-    setup({ children: 'foo' });
+    setup({ label: 'foo' });
     expect(screen.getByText('foo'));
+  });
+
+  it('test open state change on button click', () => {
+    const setStateMock = jest.fn();
+    const useStateMock: any = (useState: any) => [useState, setStateMock];
+    jest.spyOn(React, 'useState').mockImplementation(useStateMock);
+
+    const { getByRole } = setup();
+    const button = getByRole('button');
+
+    fireEvent.click(button);
+
+    expect(setStateMock).toHaveBeenCalledWith(true);
+  });
+
+  it('check contentRef classname', () => {
+    const { getByTestId } = setup();
+    const testEl = getByTestId('div-element');
+    expect(testEl.style.maxHeight).toEqual('0px');
+  });
+
+  it('check contentRef classname change on state change', () => {
+    const { getByTestId } = setup({ defaultOpen: true });
+    const testEl = getByTestId('div-element');
+    expect(testEl.style.maxHeight).toEqual('none');
   });
 });
