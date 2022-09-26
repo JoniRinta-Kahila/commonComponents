3bf1a7996eb639d10328b8e5324343df0b24f54f
JoniRinta-Kahila <joni.rintakahila@opp.eduvantaa.fi>
2022-09-26 13:18:10 +0300

    feat(radiobutton): add onChange prop

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -1,6 +1,31 @@
 import React, { useCallback } from 'react';
 import './RadioButton.scss';
+import * as CSS from 'csstype';
 
+export type fontFamilyTypes = Pick<CSS.Properties, 'fontFamily'>;
+export interface RadioButtonStyleProps {
+  colors?: {
+    checked?: {
+      circle?: string;
+      dot?: string;
+      label?: string;
+      background?: string;
+    };
+    default?: {
+      circle?: string;
+      dot?: string;
+      label?: string;
+    };
+  };
+  font?: fontFamilyTypes;
+  size?: string;
+  margin?: string | number;
+  padding?: string | number;
+  hover?: {
+    brightness?: number | string;
+    backgroundColor?: string;
+  };
+}
 export interface RadioButtonProps {
   variation?: 'dartBoard' | 'shutter';
   label?: string;
@@ -8,28 +33,8 @@ export interface RadioButtonProps {
   name?: string;
   value?: string | number | readonly string[];
   disabled?: boolean;
-  styles?: {
-    colors?: {
-      checked?: {
-        circle?: string;
-        dot?: string;
-        label?: string;
-        background?: string;
-      };
-      default?: {
-        circle?: string;
-        dot?: string;
-        label?: string;
-      };
-    };
-    size?: string;
-    margin?: string | number;
-    padding?: string | number;
-    hover?: {
-      brightness?: number | string;
-      backgroundColor?: string;
-    };
-  };
+  styles?: RadioButtonStyleProps;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const RadioButton: React.FC<RadioButtonProps> = ({
@@ -40,6 +45,7 @@ export const RadioButton: React.FC<RadioButtonProps> = ({
   disabled = false,
   variation = 'shutter',
   defaultChecked = false,
+  onChange,
 }) => {
   const genVars = useCallback(
     () => ({
@@ -62,6 +68,9 @@ export const RadioButton: React.FC<RadioButtonProps> = ({
       '--radiobutton-size': styles?.size ?? '25px',
       '--radiobutton-margin': styles?.margin ?? 0,
       '--radiobutton-padding': styles?.padding ?? 0,
+
+      // font
+      '--radiobutton-fontfamily': styles?.font?.fontFamily,
     }),
     [styles]
   );
@@ -85,6 +94,7 @@ export const RadioButton: React.FC<RadioButtonProps> = ({
         value={value}
         disabled={disabled}
         defaultChecked={defaultChecked}
+        onChange={onChange}
       />
       {variation === 'dartBoard' ? <p>{label}</p> : null}
       <span className={theme[variation].check}>
